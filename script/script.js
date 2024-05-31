c9f6240dced6c36872b8f314e0b621228e763ca4
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-05-31 16:15:28 +0200

    swegga

--- script/script.js
+++ script/script.js
@@ -1,13 +1,16 @@
-let fPrice = document.querySelectorAll('span')[0];
+const fPrice = 150.95;
+const mPrice = 180.85;
 let fTotal = document.querySelectorAll('span')[1];
-let mPrice = document.querySelectorAll('span')[2];
 let mTotal = document.querySelectorAll('span')[3];
+let total = document.querySelectorAll('span')[4]
 let inputF = document.querySelectorAll('input')[0];
 let inputM = document.querySelectorAll('input')[1];
 let button = document.querySelector('button');
 
 button.addEventListener('click',() => {
-    
+    fTotal.innerText = "R" + parseFloat(eval(`fPrice * ${inputF.value}`)).toFixed(2);
+    mTotal.innerText = "R" + parseFloat(eval(`mPrice * ${inputM.value}`)).toFixed(2);
+    total.innerText = "R" + parseFloat(eval(`fPrice * ${inputF.value} + (mPrice * ${inputM.value}) `)).toFixed(2);
 })
 
 
