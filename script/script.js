523fe5042864c188fbc9ea97689fc78440c9c4be
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-05-31 14:42:06 +0200

    swegga

---
+++ script/script.js
@@ -0,0 +1,6 @@
+let fPrice = document.querySelectorAll('span')[0];
+let fTotal = document.querySelectorAll('span')[1];
+let mPrice = document.querySelectorAll('span')[2];
+let mTotal = document.querySelectorAll('span')[3];
+
+
