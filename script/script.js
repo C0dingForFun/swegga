da38cc6a94a56328024365196e82f3ef8eceeaa2
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-05-31 15:05:40 +0200

    swegga

--- script/script.js
+++ script/script.js
@@ -2,5 +2,12 @@ let fPrice = document.querySelectorAll('span')[0];
 let fTotal = document.querySelectorAll('span')[1];
 let mPrice = document.querySelectorAll('span')[2];
 let mTotal = document.querySelectorAll('span')[3];
+let inputF = document.querySelectorAll('input')[0];
+let inputM = document.querySelectorAll('input')[1];
+let button = document.querySelector('button');
+
+button.addEventListener('click',() => {
+    
+})
 
 
